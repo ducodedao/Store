57f1a2a11f8fb54ecbf8741a5cd27a498c4bc25d
ducodedao <tongodu95@gmail.com>
2022-08-14 22:21:52 +0700

    Client Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Welcome to DStore
+
+## Available Scripts
+
+In the project directory, you can run:
+
+**Client** - `cd client yarn start`
---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from 'react'
+
+const App = () => {
+	return <Fragment>DStore</Fragment>
+}
+
+export default App
