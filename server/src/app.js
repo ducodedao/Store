9b9d5fe416a598cad9df2da448a2b0e00b7ea6e7
ducodedao <tongodu95@gmail.com>
2022-08-14 22:58:36 +0700

    Server Error Handling

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const env = require('dotenv')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const errorMiddleware = require('./api/v1/middleware/errorMiddleware')
 
 const app = express()
 
@@ -11,5 +12,8 @@ env.config()
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// Middleware for Errors
+app.use(errorMiddleware)
 
 module.exports = app
--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,13 @@ const app = require('./app')
 const env = require('dotenv')
 const connectMongoDB = require('./config/mongoDB')
 
+// Handling Uncaught Exception
+process.on('uncaughtException', (err) => {
+	console.log(`Error: ${err.message}.`)
+	console.log(`Shutting down the server due to Uncaught Exception.`)
+	process.exit(1)
+})
+
 // Config
 env.config()
 
@@ -9,5 +16,15 @@ env.config()
 connectMongoDB()
 
 const server = app.listen(process.env.PORT, () => {
-	console.log(`Server is running on http://localhost:${process.env.PORT}`)
+	console.log(`Server is running on http://localhost:${process.env.PORT}.`)
+})
+
+// Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+	console.log(`Error: ${err.message}.`)
+	console.log(`Shutting down the server due to Unhandled Promise Rejection.`)
+
+	server.close(() => {
+		process.exit(1)
+	})
 })
